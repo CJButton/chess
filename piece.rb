f9f4396517c721c5d8fa99ae908435d7fbd5799f
App Academy Student <student@appacademy.io>
2016-09-19 17:49:29 -0700

    added sliding and stepping modules

--- piece.rb
+++ piece.rb
@@ -3,10 +3,14 @@
 
 class Piece
   attr_reader :name
-  def initialize
+  def initialize(board, position)
+    @board = board
+    @position = nil
     @name = "p"
   end
 
+  def moves
 
+  end
 
 end
---
+++ sliding_piece.rb
@@ -0,0 +1,18 @@
+
+
+
+class SlidingPiece < Piece
+
+  def initialize
+    super
+  end
+
+  def moves
+
+  end
+
+  def move_dirs
+
+  end
+
+end
